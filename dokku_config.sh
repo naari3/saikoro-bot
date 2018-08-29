0e3fb7d4e2224679e8148cb703090b48e5c750df
naari3 <naari.named@gmail.com>
2018-08-29 21:49:21 +0900

    change configs

--- dokku_config.sh
+++ dokku_config.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 set -eu
 
-ssh -t dokku@dokku.db.dwango.co.jp config:set naari_bot \
-      HUBOT_HEROKU_KEEPALIVE_URL=http://naari_bot.dokku.db.dwango.co.jp \
+ssh -t dokku@dokku.hxa.me config:set saikoro-bot \
+      HUBOT_HEROKU_KEEPALIVE_URL=http://saikoro-bot.dokku.hxa.me \
       NPM_CONFIG_PRODUCTION=false \
       HUBOT_SLACK_TOKEN=$HUBOT_SLACK_TOKEN \
-      DARK_SKY_API_SECRET_KEY=$DARK_SKY_API_SECRET_KEY \
       TZ="Asia/Tokyo"
