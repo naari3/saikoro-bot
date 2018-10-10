f84cc978df148812c4ac33d7fdc87f8893df403c
naari3 <naari.named@gmail.com>
2018-10-10 21:45:46 +0900

    implemet for exp

--- src/bot/dice.js
+++ src/bot/dice.js
@@ -4,12 +4,19 @@
 const BigInt = require("big-integer");
 
 export default async robot => {
-  robot.hear(new RegExp("([1-9][\\d]*)d([1-9][\\d]*)"), res => {
-    res.send(
-      `${dice(parseInt(res.match[1]), parseInt(res.match[2])).toString()}`
-    );
+  robot.hear(new RegExp("(\\d\\.?\\d*(e\\d*)?)d(\\d\\.?\\d*(e\\d*)?)"), res => {
+    const diceNum = BigInt(res.match[1]);
+    const diceSurfaceNum = BigInt(res.match[3]);
+    if ([diceNum, diceSurfaceNum].every(i => BigInt(i).greater(0))) {
+      res.send(`${dice(diceNum, diceSurfaceNum).toString()}`);
+    }
   });
 };
 
-const dice = (a, b) =>
-  [...Array(a)].map(() => BigInt.randBetween(1, b)).reduce((c, x) => c.add(x));
+const dice = (a, b) => {
+  let sum = 0;
+  for (let i = 0; i < a; i++) {
+    sum += BigInt.randBetween(1, b);
+  }
+  return sum;
+};
